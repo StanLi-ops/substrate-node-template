b6c604fd4748a2d683d555d427214ab60df9fccd
StanLi-ops <19961024wukai@gmail.com>
2023-02-02 16:23:34 +0800

    add:新增 hook pallet练习

--- pallets/hooks/src/lib.rs
+++ pallets/hooks/src/lib.rs
@@ -4,7 +4,7 @@ pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::{pallet_prelude::*, traits::Hooks};
+	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
 
 	#[pallet::pallet]
